b1e76e1bc35aa0cc0698c5f8a9b754fdbddd996e
Aadinath1234 <aadeshsatpute375@gmail.com>
2024-04-08 22:59:55 +0530

    reduxtoolkit todoapp

---
+++ src/components/AddTodo/AddTodo.jsx
@@ -0,0 +1,34 @@
+import {useState} from 'react';
+import {useDispatch} from 'react-redux';
+import { addTodo } from '../../actions/todoActions';
+
+
+
+function AddTodo({addTodo}) {
+   
+    const [inputText, setInputText] = useState('');
+    
+
+    
+    return (
+        <div>
+             <input 
+             type="text"
+             value={inputText}
+             placeholder="add your next todo.."
+             onChange={e => setInputText(e.target.value)}
+             />
+             <button
+              onClick={() => {
+                addTodo({todoText: inputText});
+                setInputText('');
+              }
+            }>
+                Add 
+                </button>
+
+        </div>
+    )
+}
+
+export default AddTodo;  
